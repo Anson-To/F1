440cc8f50b79e4dbeca3b7f503bcf4543b25a1ab
Anson-To <55016933+Anson-To@users.noreply.github.com>
2021-10-15 00:19:44 +0800

    Add new component

---
+++ src/components/DriverItem/TheDriver.vue
@@ -0,0 +1,51 @@
+<template>
+    <base-card>
+        <base-button @click="setSelectTab('add-driver')">ADD DRIVER</base-button>
+        <base-button @click="setSelectTab('driver-list')">DRIVER LIST</base-button>
+    </base-card>
+    <component :is="selectedTab"></component>
+</template>
+
+<script>
+import BaseButton from '../UI/BaseButton.vue'
+import BaseCard from '../UI/BaseCard.vue'
+import DriverList from './DriverList.vue'
+import AddDriver from './AddDriver.vue'
+export default {
+    components: { BaseCard,BaseButton,DriverList,AddDriver },
+    data(){
+        return{
+            selectedTab:"driver-list",//default show driverList
+            lists:[
+                {
+                    id:"lewis",
+                    name:"Lewis",
+                    team:"Benz"
+                },
+                {
+                    id:"valtteri",
+                    name:"Valtteri",
+                    team:"Benz"
+                },
+            ]
+        };
+    },
+    methods:
+    {
+        setSelectTab(cmp)
+        {
+            this.selectedTab = cmp;
+        }
+    },
+    provide()
+    {
+        return {
+            list:this.lists
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
