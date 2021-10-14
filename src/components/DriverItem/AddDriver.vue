cf21a0db76e581787957b3ccbfc7591b3371d8a0
Anson-To <55016933+Anson-To@users.noreply.github.com>
2021-10-15 00:19:17 +0800

    Add new component

---
+++ src/components/DriverItem/AddDriver.vue
@@ -0,0 +1,18 @@
+<template>
+    <base-card>
+        <h2>Add new driver</h2>
+    </base-card>
+    <!-- name team -->
+</template>
+
+<script>
+import BaseCard from '../UI/BaseCard.vue'
+export default {
+  components: { BaseCard },
+    
+}
+</script>
+
+<style scoped>
+
+</style>
